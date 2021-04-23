3a789b24b4d94eafc204f93c2dfaf41993440d8c
Bruno <bruno.n.conti@gmail.com>
2021-04-23 12:00:26 -0300

    Primer commit, proyecto inicial de spring initializr

---
+++ src/main/java/ar/unnoba/edu/pdyc/apprest/AppRestApplication.java
@@ -0,0 +1,13 @@
+package ar.unnoba.edu.pdyc.apprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/unnoba/edu/pdyc/apprest/AppRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.unnoba.edu.pdyc.apprest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
